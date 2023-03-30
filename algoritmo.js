8a91ba5bd9287575f756cc5fd6165e6a7ec4db14
Roberto Alagemovits <robertomovits@gmail.com>
2023-03-30 11:59:03 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,12 @@
+document.querySelector("body").innerHTML = `
+<h1>Como servir un jugo</h1>
+<ol>
+    <li>buscar el vaso vacio</li>
+    <li>agarro la botella con jugo</li>
+    <li>verificar si tiene tapa</li>
+    <li>si tiene tapa, abrirla</li>
+    <li>sirvo el jugo hasta llenar el vaso</li>
+    <li>agarro la tapa y tapo la botella</li>
+</ol>
+<h3>rafael, antu, roberto y sofia</h3>
+`
